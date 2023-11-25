64ff8076c8c85587080479fd465ed3c26b734a14
Matthias Luger <malua007@live.de>
2023-11-25 11:44:11 +0100

    add tests if login fields are required

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,47 @@
 import { test, expect } from '@playwright/test'
 
 test('handles login', async ({ page }) => {
-    // Navigate to the page containing the login form
     await page.goto(`http://localhost:${process.env.TEST_SERVER_PORT}`)
 
-    // Fill in the email and password fields
     await page.fill('#email', process.env.TEST_EMAIL || '')
     await page.fill('#password', process.env.TEST_PASSWORD || '')
 
-    // Click the login button
     await page.click('button[type="submit"]')
 
-    // Check that the user is redirected to the '/nodes' page after successful login
     await expect(page).toHaveURL(`http://localhost:${process.env.TEST_SERVER_PORT}/nodes`)
 })
 
 test('handles login error', async ({ page }) => {
-    // Navigate to the page containing the login form
     await page.goto(`http://localhost:${process.env.TEST_SERVER_PORT}`)
 
-    // Fill in the email and password fields with incorrect data
     await page.fill('#email', 'wrong@example.com')
     await page.fill('#password', 'wrongpassword')
 
-    // Click the login button
     await page.click('button[type="submit"]')
 
     const selector = await page.waitForSelector('text=Failed to login:')
     const isVisible = await selector.isVisible()
     expect(isVisible).toBe(true)
+})
+
+test('check if email is required', async ({ page }) => {
+    await page.goto(`http://localhost:${process.env.TEST_SERVER_PORT}/login`)
+
+    await page.fill('#password', 'Test1234!')
+    await page.click('button[type="submit"]')
+
+    await expect(page.locator('#email')).toBeFocused()
+    expect(page.url()).toBe(`http://localhost:${process.env.TEST_SERVER_PORT}/login`)
+    await expect(page.locator('text=Failed to login:')).not.toBeVisible()
+})
+
+test('check if password is required', async ({ page }) => {
+    await page.goto(`http://localhost:${process.env.TEST_SERVER_PORT}/login`)
+
+    await page.fill('#email', 'test@example.com')
+    await page.click('button[type="submit"]')
+
+    await expect(page.locator('#password')).toBeFocused()
+    expect(page.url()).toBe(`http://localhost:${process.env.TEST_SERVER_PORT}/login`)
+    await expect(page.locator('text=Failed to login:')).not.toBeVisible()
 })
